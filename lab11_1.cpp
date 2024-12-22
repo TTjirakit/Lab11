581b40d330cfb7043e706fa7fdcc19f03e8ecfeb
TTjirakit <captain112006@gmail.com>
2024-12-22 18:24:28 +0700

    lab11_1 fixed 2

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(){
     cout << "Press Enter 3 times to reveal your future.\n";
     int i=0;
-    string grade[] = {"A","+B","B","+C","C","+D","D","F","W"}; 
+    string grade[] = {"A","B+","B","C+","C","D+","D","F","W"}; 
 
     while(i<3){
         cin.get();
@@ -16,7 +16,7 @@ int main(){
     srand(time(0));
     int x = rand()%9;
     string g = grade[x];
-    
+
 
     cout << "You will get " << g << " in this 261102.";
     return 0;
